0fc609252ae7db60396457b07ce1cfe40ef1259c
Adrien van den Branden <avdbranden@gmail.com>
2016-10-14 15:55:38 +0200

    adding respc and travais

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe PagesController, :type => :controller do
+  describe "GET #home" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :home
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    # it "renders the index template" do
+    #   get :index
+    #   expect(response).to render_template("index")
+    # end
+  end
+  describe "GET #programme" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :programme
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+end
