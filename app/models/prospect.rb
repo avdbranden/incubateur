38b41a71e2ac41ae5977d36216b3c4152e9fce1a
Adrien van den Branden <avdbranden@gmail.com>
2016-10-14 16:43:53 +0200

    add model validations

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -1,2 +1,7 @@
 class Prospect < ApplicationRecord
+  validates_presence_of :email
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :function
 end
