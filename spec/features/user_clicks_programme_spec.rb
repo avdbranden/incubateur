9a74fe604e64cba05cb186584866ef9ffa573c86
Adrien van den Branden <avdbranden@gmail.com>
2017-08-20 09:55:33 +0200

    add tests

--- spec/features/user_clicks_programme_spec.rb
+++ spec/features/user_clicks_programme_spec.rb
@@ -3,18 +3,12 @@ require "rails_helper"
 RSpec.feature "Simple visit and inscription", :type => :feature do
   scenario "User goes on home page" do
     visit "/"
-    expect(page).to have_text("L'Electrochoc Numérique")
+    expect(page).to have_text("L'avocat au coeur de l'innovation")
   end
-  scenario "User clicks on programme and redirected to programme page" do
+  scenario "User clicks on plus d'informations and redirected to prix page" do
     visit "/"
 
-    click_link "Voir le programme complet"
-    expect(page).to have_text("Programme provisoire")
-  end
-  scenario "User clicks on devenez partenaire and redirected to sponsors page" do
-    visit "/"
-
-    click_link "Devenez partenaire"
-    expect(page).to have_text("Ces partenaires nous soutiennent")
+    click_link "Plus d'informations"
+    expect(page).to have_text("Prix de l'Innovation")
   end
 end
