c28c8b660d221cc34f26339ca43355a71d64a633
Adrien van den Branden <avdbranden@gmail.com>
2017-09-22 13:15:41 +0200

    remove typo

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,5 +1,3 @@
-# sourced from: https://gist.github.com/esBeee/545653241530f8f2c2e16371bec56f20
-
 document.addEventListener('turbolinks:load', function(event) {
   if (typeof ga === 'function') {
     ga('set', 'location', event.data.url);
