b1b173cc233acfafe7c4f66166bd99b0e0b1ee9c
Adrien van den Branden <avdbranden@gmail.com>
2018-02-11 13:44:44 +0100

    add conference page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'prix', to: 'pages#prix'
   get 'contact', to: 'pages#contact'
   get 'equipe', to: 'pages#equipe'
+  get 'avocatconnecte', to: 'pages#avocatconnecte'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -34,4 +34,12 @@ RSpec.describe PagesController, :type => :controller do
     end
   end
 
+  describe "GET #avocatconnecte" do
+    it "avocatconnecte successfully with an HTTP 200 status code" do
+      get :avocatconnecte
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
